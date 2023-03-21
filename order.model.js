d971c0c2b04cad5803932f4ecfb659ad0aaa5e1f
x-chandral49 <x-chandral49@msasafety.com>
2023-03-21 22:24:48 +0530

    one to description contraint remove

--- order.model.js
+++ order.model.js
@@ -6,7 +6,6 @@ module.exports = (sequelize, Sequelize) => {
         },
         description: {
             type: Sequelize.STRING,
-            primaryKey:true,
             allowNull: false,
         },
         active:{
